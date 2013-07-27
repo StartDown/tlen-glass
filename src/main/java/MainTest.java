9d528eaa3e2ac2d3da802fb350bcab22587a5695
Yarix <Yarikx@gmail.com>
2013-07-27 17:36:47 +0300

    add tlen processing class

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,12 @@
+import com.google.glassware.ImageProcessingUtil;
+
+/**
+ * Created by yarik on 7/27/13.
+ */
+public class MainTest {
+    public static void main(String[] args){
+        String url = "http://img216.imageshack.us/img216/3698/huita.jpg";
+        ImageProcessingUtil.tlenifyImage(2, url);
+
+    }
+}
---
+++ src/main/java/com/google/glassware/ImageProcessingUtil.java
@@ -0,0 +1,44 @@
+package com.google.glassware;
+
+import com.github.kevinsawicki.http.HttpRequest;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by yarik on 7/27/13.
+ */
+public class ImageProcessingUtil {
+
+    static interface TlenCallback{
+        public void tlen(String imageUrl);
+    }
+
+    private static final Pattern imagePattern = Pattern.compile("href=\"(.*)\"");
+
+    public static void tlenifyImage(int bgNum, String imageUrl, TlenCallback callback) {
+
+        byte[] image = HttpRequest.get(imageUrl).bytes();
+
+        HttpRequest request = HttpRequest.post("http://tlenta.ru/tlenify.php");
+        request.part("MAX_FILE_SIZE", "2000000");
+
+        InputStream is = new ByteArrayInputStream(image);
+        request.part("img", "foobar.jpg", "image/jpeg", is);
+        request.part("fon", bgNum);
+        request.part("txt", "");
+        if (request.ok()) {
+            String body = request.body();
+            Matcher matcher = imagePattern.matcher(body);
+
+            if(matcher.find()){
+                String url = matcher.group(1);
+                System.out.println(url);
+                callback.tlen(url);
+            }
+
+        }
+    }
+}
